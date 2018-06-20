c71a188fd9028336e88b1ca8f3fa4c6cacd5b593
Aakash Tyagi <aakashtyagi1996@outlook.com>
2018-06-20 23:47:37 +0530

    Using Multithreading !

---
+++ ThumbnailCreater/src/com/java/ThumbnailCreator/main/ConversionThread.java
@@ -0,0 +1,27 @@
+package com.java.ThumbnailCreator.main;
+
+import javax.swing.JOptionPane;
+
+import com.java.ThumbnailCreater.classes.ImageConverter;
+
+
+
+public class ConversionThread extends Thread {
+
+	ImageConverter imageConverter;
+
+	public ConversionThread(ImageConverter imageConverter) {
+		this.imageConverter = imageConverter;
+	}
+
+	public void run() {
+		String result = imageConverter.convertImages();
+		JOptionPane.showMessageDialog(null, result);
+		// Thread sleeps for 4 milis.
+		try {
+			Thread.sleep(4);
+		} catch (InterruptedException e) {
+			return;
+		}
+	}
+}
